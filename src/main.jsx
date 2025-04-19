b05b4add319db13197aa203ebb6cd55aaacf7c79
Yogesh Palve <dnyyogesh@gmail.com>
2025-04-20 00:22:35 +0530

    store added to main application

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,15 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { ThemeProvider } from "./components/theme-provider";
+import { Provider } from "react-redux";
+import { appStore } from "./app/store";
 
 createRoot(document.getElementById("root")).render(
   <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
     <StrictMode>
-      <App />
+      <Provider store={appStore}>
+        <App />
+      </Provider>
     </StrictMode>
   </ThemeProvider>
 );
